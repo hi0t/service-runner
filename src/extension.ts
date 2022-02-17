9635fbbe51903ea09cdd8045657c27661ce1c745
hi0t <27971344+hi0t@users.noreply.github.com>
2022-02-17 18:40:14 +0200

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,7 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() { }
